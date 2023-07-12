e5eea04a18635841fc7fe81aa70f338865a743e0
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-12 10:59:36 -0700

    added start quiz button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,41 @@
 // Define variables
 var timeEl = document.querySelector(".time");
-var secondsLeft = 60;
+var secondsLeft = 15;
+var startButton = document.querySelector("#startQuiz");
 
 
-// Set the displayed time. When the timer reaches 0, execute the sendMessage function.
+// Quiz timer function.
 function setTime() {
-    var timerInterval = setInterval(function(){
+    var timerInterval = setInterval(function () {
         secondsLeft--;
+        // Display countdown.
         timeEl.textContent = secondsLeft + " seconds remaining";
-        
-        if(secondsLeft===0){
+
+        // When the timer reaches < 10, change text to red.
+        if (secondsLeft < 10) {
+            timeEl.style.color = "red";
+        }
+
+        // When the timer reaches 0, execute the sendMessage function.
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
             sendMessage();
         }
+        // Hide the Start Quiz button
+        startButton.style.display = "none";
     }, 1000)
 }
 
-// Message to be displayed when the timer in the setTime() function reaches 0.
+// Display message when the timer in the setTime() function reaches 0, and change text color back to black.
 function sendMessage() {
     timeEl.textContent = "Pencils down! Quiz over!"
+    timeEl.style.color = "black";
 }
 
-setTime();
+// Start Quiz button event listener for launching the quiz
+function startQuiz() {
+    startButton.addEventListener("click", setTime);
+}
+
+
+startQuiz();
