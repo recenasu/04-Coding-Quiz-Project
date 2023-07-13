9b3319962d194250cda6586f4f977a7e78164b85
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-13 16:15:22 -0700

    added the question swap out elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Define variables
 var timeEl = document.querySelector(".time");
-var secondsLeft = 15;
+var secondsLeft = 35;
 var startButton = document.querySelector("#startQuiz");
 var questionField = document.createElement("h4");
 var answerList = document.createElement("ul");
@@ -9,59 +9,129 @@ var answer2Field = document.createElement("li");
 var answer3Field = document.createElement("li");
 var answer4Field = document.createElement("li");
 var resultField = document.createElement("h4");
-var rightAnswerKey = "3";
+var rightAnswerKey = "";
+var questionCounter = 0;
+var finalScore = "";
+
+// Question bank. Each question object contains a question, answers, and the correct answer identified.
+var question001 = {
+    questionDisplayed: "What is your name?",
+    answerDisplayed1: "Sir Lancelot",
+    answerDisplayed2: "Sir Gawain",
+    answerDisplayed3: "Sir Percival",
+    answerDisplayed4: "Sir Robin",
+    correctAnswer: "1"
+};
+
+var question002 = {
+    questionDisplayed: "What is your quest?",
+    answerDisplayed1: "To seek the grail",
+    answerDisplayed2: "To rule all of Briton",
+    answerDisplayed3: "To reach Camelot",
+    answerDisplayed4: "To reach the Castle aaaaghhh",
+    correctAnswer: "3"
+};
+
+var question003 = {
+    questionDisplayed: "What is your favorite color?",
+    answerDisplayed1: "Blue",
+    answerDisplayed2: "No, yellow",
+    answerDisplayed3: "Red",
+    answerDisplayed4: "Green",
+    correctAnswer: "2"
+};
+
+var question004 = {
+    questionDisplayed: "What is your favorite car?",
+    answerDisplayed1: "Blue",
+    answerDisplayed2: "No, yellow",
+    answerDisplayed3: "Red",
+    answerDisplayed4: "Green",
+    correctAnswer: "2"
+};
+
+var question005 = {
+    questionDisplayed: "What is your favorite boat?",
+    answerDisplayed1: "Blue",
+    answerDisplayed2: "No, yellow",
+    answerDisplayed3: "Red",
+    answerDisplayed4: "Green",
+    correctAnswer: "2"
+};
+
+var question006 = {
+    questionDisplayed: "What is your favorite food?",
+    answerDisplayed1: "Blue",
+    answerDisplayed2: "No, yellow",
+    answerDisplayed3: "Red",
+    answerDisplayed4: "Green",
+    correctAnswer: "2"
+};
+
+// Array variable containing the question bank objects.
+var questionBank = [question001, question002, question003, question004, question005, question006];
+
 
 
 
 // Quiz timer function.
 function setTime() {
+
+    // Hide the Start Quiz button
+    startButton.style.display = "none";
+
+
+
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        // Display countdown.
-        timeEl.textContent = secondsLeft + " seconds remaining";
 
+        if (questionCounter >= questionBank.length || secondsLeft <= 0) {
+            // stop the timer
+            clearInterval(timerInterval);
+            // Display the final score
+            timeEl.textContent = "Quiz over! \n Your final score is " + secondsLeft + "!!";
+            timeEl.style.color = "black";
+            // Remove the quiz elements
+            document.body.children[3].remove(questionField);
+            document.body.children[3].remove(answerList);
+            document.body.children[3].remove(answer1Field);
+            document.body.children[3].remove(answer2Field);
+            document.body.children[3].remove(answer3Field);
+            document.body.children[3].remove(answer4Field);
+            document.body.children[3].remove(resultField);
+            sendMessage();
+
+        } else {
+            // Display countdown.
+            timeEl.textContent = secondsLeft + " seconds remaining";
+            // Display the quiz elements
+            document.body.children[3].appendChild(questionField);
+            document.body.children[3].appendChild(answerList);
+            document.body.children[3].appendChild(answer1Field);
+            document.body.children[3].appendChild(answer2Field);
+            document.body.children[3].appendChild(answer3Field);
+            document.body.children[3].appendChild(answer4Field);
+            document.body.children[3].appendChild(resultField);
+
+            nextQuestion(questionCounter);
+
+        }
         // When the timer reaches < 10, change text to red.
         if (secondsLeft < 10) {
             timeEl.style.color = "red";
         }
 
-        // When the timer reaches 0, execute the sendMessage function.
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-        // Hide the Start Quiz button
-        startButton.style.display = "none";
+    }, 1000);
 
-        // Display the quiz elements
-        document.body.children[3].appendChild(questionField);
-        document.body.children[3].appendChild(answerList);
-        document.body.children[3].appendChild(answer1Field);
-        document.body.children[3].appendChild(answer2Field);
-        document.body.children[3].appendChild(answer3Field);
-        document.body.children[3].appendChild(answer4Field);
-        document.body.children[3].appendChild(resultField);
 
-        // populate the question and answer fields.
-        questionField.textContent = "question";
-        answerList.textContent = "Select one answer";
-        answer1Field.textContent = "answer";
-        answer2Field.textContent = "answer";
-        answer3Field.textContent = "answer";
-        answer4Field.textContent = "answer";
-
-        // Add event listeners for the answers to be selectable and execute either the wrongAnswer or rightAnswer function.
-        setAnswers()
-
-    }, 1000)
 }
 
-function setAnswers () {
+function setAnswers() {
     if (rightAnswerKey === "1") {
         answer1Field.addEventListener("click", rightAnswer);
         answer2Field.addEventListener("click", wrongAnswer);
         answer3Field.addEventListener("click", wrongAnswer);
-        answer4Field.addEventListener("click", wrongAnswer);        
+        answer4Field.addEventListener("click", wrongAnswer);
     } else if (rightAnswerKey === "2") {
         answer1Field.addEventListener("click", wrongAnswer);
         answer2Field.addEventListener("click", rightAnswer);
@@ -78,74 +148,58 @@ function setAnswers () {
         answer3Field.addEventListener("click", wrongAnswer);
         answer4Field.addEventListener("click", rightAnswer);
     }
+
 }
 
 
-
+// Wrong answer actions. Display an "INCORRECT" message. Decrement timer as a penalty against the final score.
 function wrongAnswer() {
     resultField.textContent = "INCORRECT!";
     resultField.style.color = "red";
+    // Increment the question counter
+    questionCounter++;
+    // Decrement timer
+    secondsLeft = secondsLeft - 10;
+    nextQuestion(questionCounter);
 }
 
 function rightAnswer() {
     resultField.textContent = "CORRECT!";
     resultField.style.color = "green";
+    // Increment the question counter
+    questionCounter++;
+    // Go to the next question
+    nextQuestion(questionCounter);
+}
+
+function nextQuestion(questionCounter) {
+
+    // Pull data from the next question object to populate the question and answer fields and obtain the right answer.
+    questionField.textContent = questionBank[questionCounter].questionDisplayed;
+    answerList.textContent = "Select one answer"
+    answer1Field.textContent = questionBank[questionCounter].answerDisplayed1;
+    answer2Field.textContent = questionBank[questionCounter].answerDisplayed2;
+    answer3Field.textContent = questionBank[questionCounter].answerDisplayed3;
+    answer4Field.textContent = questionBank[questionCounter].answerDisplayed4;
+    rightAnswerKey = questionBank[questionCounter].correctAnswer;
+
+
+    // Add event listeners for the answers to be selectable and execute either the wrongAnswer or rightAnswer function based on the value of the rightAnswerKey variable.
+    setAnswers();
 }
 
 // Display message when the timer in the setTime() function reaches 0, and change text color back to black.
 function sendMessage() {
-    timeEl.textContent = "Pencils down! Quiz over!";
-    timeEl.style.color = "black";
+
+
+
 }
 
 // Start Quiz button event listener for launching the quiz
 function startQuiz() {
     startButton.addEventListener("click", setTime);
+
 }
 
 
 startQuiz();
-
-
-
-
-
-// ***This is all draft code***
-// Question Objects
-
-// var question1 = {
-//     question: "words",
-//     answer1: "words",
-//     answer2: "words",
-//     answer3: "words",
-//     answer4: "words",
-//     rightAnswer: "1"
-// }
-
-// var questionDisplayed = questionObject.question
-// var answer1Displayed = questionObject.answer1
-// var answer2Displayed = questionObject.answer2
-// var answer3Displayed = questionObject.answer3
-// var answer4Displayed = questionObject.answer4
-// var correctAnswer = questionObject.rightAnswer;
-
-
-
-// answer1Button.addEventListener("click", wrongAnswer);
-// answer2Button.addEventListener("click", wrongAnswer);
-// answer3Button.addEventListener("click", wrongAnswer);
-// answer4Button.addEventListener("click", rightAnswer);
-
-// create a for loop for cycling through the question objects
-// for (i = 1; i < 11;)
-
-//     function nextQuestion(objectName) {
-//         questionDisplayed.text(objectName.question);
-//     }
-
-// function wrongAnswer() { }
-
-// function rightAnswer() { }
-
-// // ***End draft code***
-
